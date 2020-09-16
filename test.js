0d6f58daba534e1134311d68320309563ba32b50
Dim <dimitri.kamenski@gmail.com>
2020-09-16 11:13:36 +1000

    feat: test adjustments
    - change test structure from boolean to object

--- test.js
+++ test.js
@@ -1,8 +1,8 @@
 it('should pass', ()=>{
+	expect({state: true}).toMatchObject({state: true});
 	expect(false).tobe(false);
 });
 
 it('should console log and really do absolutely nothing', ()=>{
 	console.log('this is a useless test');
-
 });

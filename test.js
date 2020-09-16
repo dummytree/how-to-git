57e7de0aa933e6f3e73195258081d4011f8e3131
Dim <dimitri.kamenski@gmail.com>
2020-09-16 11:11:20 +1000

    feat: test adjustment
    - change test structure from true to false
    - add useless test

--- test.js
+++ test.js
@@ -1,3 +1,8 @@
 it('should pass', ()=>{
-	expect(true).tobe(true);
+	expect(false).tobe(false);
+});
+
+it('should console log and really do absolutely nothing', ()=>{
+	console.log('this is a useless test');
+
 });
